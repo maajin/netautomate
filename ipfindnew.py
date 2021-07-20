2615a3f0062360c5b6858f29c8021ac3f22c5f42
Mohammad Sharik <boosaiyan@gmail.com>
2021-07-20 12:53:23 +0530

    Update ipfindnew.py

--- ipfindnew.py
+++ ipfindnew.py
@@ -1,15 +1,16 @@
-import openpyxl
-import ipaddress
+import pandas as pd
 
-path = "D:\Python Scripts\subnets.xlsx"
- 
-wb_obj = openpyxl.load_workbook(path)
-sheet_obj = wb_obj.active
+df = pd.read_excel("D:\Python Scripts\subnets.xlsx", sheet_name=0)
+# insert the name of the column as a string in brackets
+list1 = list(df['Subnt']) 
+
+
+NETWORKS = list1
 
 ADDRESSES = input(' Please enter ip: ')
 
 for ip in ADDRESSES:
-    for net in sheet_obj:
+    for net in NETWORKS:
         if ip in net:
             print('{}\nis on {}'.format(ip, net))
             break
