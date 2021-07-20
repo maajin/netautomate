aa5809209e82c7cf67d5e62b959704586d426400
Mohammad Sharik <boosaiyan@gmail.com>
2021-07-20 21:16:43 +0530

    Update ipfindnew.py

--- ipfindnew.py
+++ ipfindnew.py
@@ -1,19 +1,19 @@
+
 import pandas as pd
+import ipaddress
 
-df = pd.read_excel("D:\Python Scripts\subnets.xlsx", sheet_name=0)
-# insert the name of the column as a string in brackets
+df = pd.read_excel("./subnets.xlsx", sheet_name=0)
 list1 = list(df['Subnt']) 
+tmp = input(' Please enter ip: ')
 
-
-NETWORKS = list1
-
-ADDRESSES = input(' Please enter ip: ')
-
-for ip in ADDRESSES:
-    for net in NETWORKS:
-        if ip in net:
-            print('{}\nis on {}'.format(ip, net))
-            break
-    else:
-        print('{}\nis not on a known network'.format(ip))
-    print()
+try:
+        ADDRESSES = ipaddress.ip_address(tmp)
+        for item in list1:
+                val = ipaddress.ip_address(tmp) in ipaddress.ip_network(item)
+                if val == True:
+                        print(f'\nIP: {ADDRESSES} found on subnet -> {item}')
+                        break
+        else:
+                print(f'\nIP: {ADDRESSES} NOT found in any subnet.')
+except Exception as e:
+        print(f'Error -> {str(e)}')
