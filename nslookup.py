d4a645ec6fcda1221f654050ec37359fa825cd46
Mohammad Sharik <boosaiyan@gmail.com>
2022-11-24 14:24:09 +0530

    Add files via upload

---
+++ nslookup.py
@@ -0,0 +1,15 @@
+import socket
+import pandas as pd
+
+file =open ('dns.txt', 'r')
+domain_list = []
+
+for x in file.readlines():
+    domain_list.append(x.rstrip())
+
+for y in domain_list:
+    try:
+        print (socket.gethostbyname(y))
+    except:
+        print ("Unable to resolve")
+
