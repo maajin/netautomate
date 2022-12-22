fd052fc0ddeb0b73e246bc6dba60ee6d4614851d
Mohammad Sharik <boosaiyan@gmail.com>
2022-12-22 21:46:05 +0530

    Update firemongetconfig.py

--- firemongetconfig.py
+++ firemongetconfig.py
@@ -1,42 +1,4 @@
-import requests
 
-# Set the base URL for the Firemon API
-base_url = "https://<firemon-server>/api/1.0"
-
-# Set the API endpoint for retrieving firewall configurations
-endpoint = "/configurations"
-
-# Set the API key for authenticating with the Firemon API
-api_key = "<api-key>"
-
-# Set the headers for the API request
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Api-Key {api_key}",
-}
-
-# Send the API request to retrieve the firewall configurations
-response = requests.get(base_url + endpoint, headers=headers)
-
-# Check the status code of the response
-if response.status_code == 200:
-    # If the request was successful, the firewall configurations will be in the response data
-    firewall_configurations = response.json()
-    # You can then iterate through the firewall configurations and save them to a file or do whatever else you need to do with them
-    for firewall_config in firewall_configurations:
-        print(firewall_config)
-else:
-    # If the request was not successful, there may be an error in the request or the API may be down
-    print("An error occurred while retrieving the firewall configurations:", response.text)
-Note that this is just a basic example and you may need to modify it to fit your specific needs and requirements. You may also need to consult the Firemon API documentation for more information on the available endpoints and parameters.
-
-
-
-
-Write a python script to download all firewall configurations from Firemon and store it ins a folder.
-To download all firewall configurations from Firemon and store them in a folder, you can use the following Python script:
-
-Copy code
 import requests
 import os
 
